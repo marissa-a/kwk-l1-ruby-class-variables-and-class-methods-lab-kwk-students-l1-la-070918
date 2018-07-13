8bc90422ff9fc19b42149ca04a4dbe22eea0f609
marissa-a <marissa.andrade@verizon.net>
2018-07-13 20:34:11 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,25 @@
+class Song
+  attr_accessor: :name, :artist, :genre
+  
+  @@count = 0
+  @@genres = [] 
+  @@artists = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @@count += 1
+    @@genre = genre
+  end
+end
+  
+def count
+  @@count = .count
+end
 
+def genres
+  @@genres = genres
+end 
+
+def artist
+  @@artist = artist
+end
